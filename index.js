5acaf838612a120ffa90a2c67c28eb9865d52661
BJG87 <bgrear@stvps.co.uk>
2021-10-16 14:45:04 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Import stylesheets
 import './style.css';
+import firebase from 'firebase';
+import { GoogleAuthProvider } from 'firebase/auth';
+const provider = new GoogleAuthProvider();
 
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
