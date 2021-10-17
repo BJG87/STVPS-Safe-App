781444afa46cdead4267dfe704d089e21bb52575
BJG87 <bgrear@stvps.co.uk>
2021-10-17 20:24:33 +0100

    2 files modified

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ import firebase from 'firebase';
 import { GoogleAuthProvider } from 'firebase/auth';
 const provider = new GoogleAuthProvider();
 
+
+/*
+https://docs.google.com/forms/d/e/1FAIpQLSdK7iRUbqJWlhBGD1HfRyaQ7ZoYmG8I8hgk1s_f2HSjhjg-4Q/viewform?usp=pp_url&entry.969926080=bgrear2.307@stvps.co.uk&entry.691698998=Brett+G'rear
+
+*/
+
+function submitForm(form) {
+  
+}
+
+
+
+
+
+
+
+
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
@@ -12,4 +29,16 @@ const provider = new GoogleAuthProvider();
 <script src="https://www.gstatic.com/firebasejs/ui/4.8.1/firebase-ui-auth.js"></script>
   <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/4.8.1/firebase-ui-auth.css" />
 
+*/
+
+
+/*
+<div id="formDiv">
+        <form id="form">
+          <label for="name">Name</label>
+          <input type="text" name="name" />
+          <br />
+          <input type="button" id="send" value="Send" />
+        </form>
+      </div>
 */
