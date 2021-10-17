aef45590612818733dcf55fb4c633dfe37f17218
BJG87 <bgrear@stvps.co.uk>
2021-10-17 19:52:45 +0100

    2 files modified

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const provider = new GoogleAuthProvider();
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+/*
+<script src="https://www.gstatic.com/firebasejs/ui/4.8.1/firebase-ui-auth.js"></script>
+  <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/4.8.1/firebase-ui-auth.css" />
+
+*/
