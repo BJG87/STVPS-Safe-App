3e187db688d4812fe5c6f75e53997216f085c28c
BJG87 <bgrear@stvps.co.uk>
2021-10-18 22:01:58 +0100

    3 files modified

--- index.js
+++ index.js
@@ -1,10 +1,191 @@
 // Import stylesheets
 import './style.css';
-import firebase from 'firebase';
-import { GoogleAuthProvider } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+//import firebase from 'firebase';
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
 import { getFirestore, addDoc, collection } from 'firebase/firestore';
-const provider = new GoogleAuthProvider();
+import * as firebaseui from 'firebaseui';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyCVKn4k0QWRpEOJHgbl7ALgYUNV7o9kwGQ',
+  authDomain: 'stvps-safe-app.firebaseapp.com',
+  projectId: 'stvps-safe-app',
+  storageBucket: 'stvps-safe-app.appspot.com',
+  messagingSenderId: '628432699289',
+  appId: '1:628432699289:web:5dacd9e8687b0add921d08',
+};
+console.log('Hide Dashboard');
+document.getElementById('dashboard').style.display = 'none';
+console.log('Setup Event listener');
+document.getElementById('login').addEventListener('click', GoogleLogin);
+
+function GoogleLogin() {
+  console.log('Login Btn Call');
+  initializeApp(firebaseConfig);
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
+
+//<script src="https://www.gstatic.com/firebasejs/8.0.1/firebase-app.js"></script>
+
+//<script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-auth.js"></script>
+
+//<!-- TODO: Add SDKs for Firebase products that you want to use
+//https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// Your web app's Firebase configuration
+/*
+
+let db, auth;
+async function main() {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyCVKn4k0QWRpEOJHgbl7ALgYUNV7o9kwGQ',
+    authDomain: 'stvps-safe-app.firebaseapp.com',
+    projectId: 'stvps-safe-app',
+    storageBucket: 'stvps-safe-app.appspot.com',
+    messagingSenderId: '628432699289',
+    appId: '1:628432699289:web:5dacd9e8687b0add921d08',
+  };
+  try {
+    if (firebaseConfig && firebaseConfig.apiKey) {
+      initializeApp(firebaseConfig);
+    }
+    auth = getAuth();
+    db = getFirestore();
+  } catch (e) {}
+}
+main();
+
+
+// Initialize Firebase
+var firebase;
+//firebase.initializeApp(firebaseConfig);
+//initializeApp(firebaseConfig)
+document.getElementById('dashboard').style.display = 'none';
+
+document.getElementById('login').addEventListener('click', GoogleLogin);
+document.getElementById('logout').addEventListener('click', LogoutUser);
+
+let provider = new firebase.auth.GoogleAuthProvider();
+
+function GoogleLogin() {
+  console.log('Login Btn Call');
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((res) => {
+      console.log(res.user);
+      document.getElementById('LoginScreen').style.display = 'none';
+      document.getElementById('dashboard').style.display = 'block';
+      //showUserDetails(res.user);
+      showForm(res.user);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
+function showUserDetails(user) {
+  document.getElementById('userDetails').innerHTML = `
+      <img src="${user.photoURL}" style="width:10%">
+      <p>Name: ${user.displayName}</p>
+      <p>Email: ${user.email}</p>
+    `;
+}
+
+function showForm(user) {
+  var domain = user.email.split('@')[1];
+  console.log(domain);
+  if (domain == 'stvps.co.uk') {
+    //Show Form
+    var html = '';
+    html += '<form id="form">';
+    html += '<label for="name">Name</label>';
+    html += '<input type="text" name="name" />';
+    html += '<br>';
+    html += '<button type="submit" id="send">Send</button>';
+    html += '</form>';
+    document.getElementById('formDiv').innerHTML = html;
+
+    var form = document.getElementById('form');
+    var db = getFirestore();
+    form.addEventListener('submit', async (e) => {
+      // Prevent the default form redirect
+      e.preventDefault();
+      // Write a new message to the database collection "guestbook"
+      addDoc(collection(db, 'messages'), {
+        //text: input.value,
+        //timestamp: Date.now(),
+        //name: auth.currentUser.displayName,
+        //userId: auth.currentUser.uid
+        text: 'FirstTest',
+        timestamp: Date.now(),
+      });
+      return false;
+    });
+  } else {
+    //Show Not Authorised
+    var html = '';
+    html +=
+      "You are not authorised to use this app.  It is only available to students at St Vincent's.";
+    document.getElementById('formDiv').innerHTML = html;
+  }
+}
+
+//Build function to send email here
+
+function checkAuthState() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      document.getElementById('LoginScreen').style.display = 'none';
+      document.getElementById('dashboard').style.display = 'block';
+      showUserDetails(user);
+    } else {
+    }
+  });
+}
+
+function LogoutUser() {
+  console.log('Logout Btn Call');
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      document.getElementById('LoginScreen').style.display = 'block';
+      document.getElementById('dashboard').style.display = 'none';
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+checkAuthState();
+*/
 /*
 https://docs.google.com/forms/d/e/1FAIpQLSdK7iRUbqJWlhBGD1HfRyaQ7ZoYmG8I8hgk1s_f2HSjhjg-4Q/viewform?usp=pp_url&entry.969926080=bgrear2.307@stvps.co.uk&entry.691698998=Brett+G'rear
 
@@ -32,3 +213,128 @@ function submitForm(form) {}
         </form>
       </div>
 */
+
+/*
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="https://www.gstatic.com/firebasejs/8.0.1/firebase-app.js"></script>
+
+    <script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-auth.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+    <script>
+      // Your web app's Firebase configuration
+      const firebaseConfig = {
+        apiKey: 'AIzaSyCVKn4k0QWRpEOJHgbl7ALgYUNV7o9kwGQ',
+        authDomain: 'stvps-safe-app.firebaseapp.com',
+        projectId: 'stvps-safe-app',
+        storageBucket: 'stvps-safe-app.appspot.com',
+        messagingSenderId: '628432699289',
+        appId: '1:628432699289:web:5dacd9e8687b0add921d08',
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+      document.getElementById('dashboard').style.display = 'none';
+
+      document.getElementById('login').addEventListener('click', GoogleLogin);
+      document.getElementById('logout').addEventListener('click', LogoutUser);
+
+      let provider = new firebase.auth.GoogleAuthProvider();
+
+      function GoogleLogin() {
+        console.log('Login Btn Call');
+        firebase
+          .auth()
+          .signInWithPopup(provider)
+          .then((res) => {
+            console.log(res.user);
+            document.getElementById('LoginScreen').style.display = 'none';
+            document.getElementById('dashboard').style.display = 'block';
+            //showUserDetails(res.user);
+            showForm(res.user);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+
+      function showUserDetails(user) {
+        document.getElementById('userDetails').innerHTML = `
+          <img src="${user.photoURL}" style="width:10%">
+          <p>Name: ${user.displayName}</p>
+          <p>Email: ${user.email}</p>
+        `;
+      }
+
+      function showForm(user) {
+        var domain = user.email.split('@')[1];
+        console.log(domain);
+        if (domain == 'stvps.co.uk') {
+          //Show Form
+          var html = '';
+          html += '<form id="form">';
+          html += '<label for="name">Name</label>';
+          html += '<input type="text" name="name" />';
+          html += '<br>';
+          html += '<button type="submit" id="send">Send</button>';
+          html += '</form>';
+          document.getElementById('formDiv').innerHTML = html;
+          
+          var form = document.getElementById('form');
+          var db = getFirestore();
+          form.addEventListener('submit', async e => {
+            // Prevent the default form redirect
+            e.preventDefault();
+            // Write a new message to the database collection "guestbook"
+            addDoc(collection(db, 'messages'), {
+              //text: input.value,
+              //timestamp: Date.now(),
+              //name: auth.currentUser.displayName,
+              //userId: auth.currentUser.uid
+              text: 'FirstTest',
+              timestamp: Date.now()
+            });
+            return false;
+          });
+
+        } else {
+          //Show Not Authorised
+          var html = '';
+          html +=
+            "You are not authorised to use this app.  It is only available to students at St Vincent's.";
+          document.getElementById('formDiv').innerHTML = html;
+        }
+      }
+
+      //Build function to send email here
+
+      function checkAuthState() {
+        firebase.auth().onAuthStateChanged((user) => {
+          if (user) {
+            document.getElementById('LoginScreen').style.display = 'none';
+            document.getElementById('dashboard').style.display = 'block';
+            showUserDetails(user);
+          } else {
+          }
+        });
+      }
+
+      function LogoutUser() {
+        console.log('Logout Btn Call');
+        firebase
+          .auth()
+          .signOut()
+          .then(() => {
+            document.getElementById('LoginScreen').style.display = 'block';
+            document.getElementById('dashboard').style.display = 'none';
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+      checkAuthState();
+    </script>
+
+    */
