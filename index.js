92304208f3db91c8ede90d658b22d8f30ea0e95d
BJG87 <bgrear@stvps.co.uk>
2021-10-30 19:49:42 +0100

    1 file modified

--- index.js
+++ index.js
@@ -53,8 +53,29 @@ let provider = new GoogleAuthProvider();
 
 function GoogleLogin() {
   console.log('Login Btn Call');
-  const auth = getAuth(firebaseApp);
-  auth.signInWithPopup(provider);
+  //const auth = getAuth(firebaseApp);
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+
+  //auth.signInWithPopup(provider);
   //onAuthStateChanged(auth, user => {
   // Check for user status
   //});
